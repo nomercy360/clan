0a3d1317e880ce9ad109c1cb0c40c6ad7f47ee32
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2023-11-24 17:12:02 +0700

    initial

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'dusty-rose': '#976D6D',
+        red: '#FF3D00',
+        green: '#009274',
+        gray: '#BCBCBC',
+      },
+    },
+  },
+  plugins: [],
+};
